c599fe8cd9e4e9d4b4cdacca82d715c3b575267c
masaru-suzuki <masaru.suzuki.yamagata@gmail.com>
2021-03-30 09:42:08 +0900

    fix test

--- src/uikit/SubmitBtn.jsx
+++ src/uikit/SubmitBtn.jsx
@@ -1,25 +1,19 @@
-import React from "react";
-import { makeStyles } from "@material-ui/core/styles";
-import { Button } from "@material-ui/core";
+import React from 'react'
+import {makeStyles} from '@material-ui/core/styles'
+import {Button} from '@material-ui/core'
 const useStyles = makeStyles((theme) => ({
-	btn: {
-		marginBottom: 24,
-	},
-}));
+  btn: {
+    marginBottom: 24,
+  },
+}))
 
-const SubmitBtn = ({ value, updateFireStore }) => {
-	const classes = useStyles();
-	return (
-		<Button
-			fullWidth
-			variant="contained"
-			color="primary"
-			className={classes.btn}
-			onClick={updateFireStore}
-		>
-			{value}
-		</Button>
-	);
-};
+const SubmitBtn = ({value, updateFireStore}) => {
+  const classes = useStyles()
+  return (
+    <Button fullWidth variant="contained" color="primary" className={classes.btn} onClick={updateFireStore}>
+      {value}
+    </Button>
+  )
+}
 
-export default SubmitBtn;
+export default SubmitBtn
