266c16aa62e9ea483cdf2807b76408a9028183e1
Hajime Suzuki <hajime.doublebass@gmail.com>
2021-03-31 15:15:58 +0200

    fix eslint errors

--- src/uikit/SubmitBtn.jsx
+++ src/uikit/SubmitBtn.jsx
@@ -1,13 +1,13 @@
 import React from 'react'
-import {makeStyles} from '@material-ui/core/styles'
-import {Button} from '@material-ui/core'
+import { makeStyles } from '@material-ui/core/styles'
+import { Button } from '@material-ui/core'
 const useStyles = makeStyles((theme) => ({
   btn: {
     marginBottom: 24,
   },
 }))
 
-const SubmitBtn = ({value, updateFireStore}) => {
+const SubmitBtn = ({ value, updateFireStore }) => {
   const classes = useStyles()
   return (
     <Button fullWidth variant="contained" color="primary" className={classes.btn} onClick={updateFireStore}>
